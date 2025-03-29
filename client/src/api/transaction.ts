f83ab354516aafa57960283f1aeeb2bd6fe4817f
eerie-cson <ericson.sacdalan.4@gmail.com>
2025-03-29 13:23:05 +0800

    chore: separate client to another repository

--- client/src/api/transaction.ts
+++
@@ -1,5 +0,0 @@
-export const fetchTransactions = async () => {
-  const response = await fetch("/api/transactions");
-  if (!response.ok) throw new Error("Failed to fetch transactions");
-  return response.json();
-};
